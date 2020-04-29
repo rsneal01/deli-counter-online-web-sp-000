1294c19b28863ea08ee22714283d33980f30df00
Bobby Neal <rsneal01@gmail.com>
2020-04-29 16:23:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,10 @@ def line(deli)
   else 
     current_line = "The line is currently:"
     deli.each.with_index(1) do |customer, index|
-  puts current_line << " #{index}. #{customer}"
-end
-end
+      puts current_line << " #{index}. #{customer}"
+    end
+  end
+  
 #{index + 1}
   #     puts "The line is currently: 1. #{deli[0]} 2. #{deli[1]} 3. #{deli[2]} 4. #{deli[3]} 5. #{deli[4]} 6. #{deli[5]} 7. #{deli[6]} 8. #{deli[7]} 9. #{deli[8]} 10. #{deli[9]}"  
   # end
