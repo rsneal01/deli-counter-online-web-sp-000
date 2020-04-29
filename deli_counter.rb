d424a8992c8d5e18619ecda17d2c3c5c1c0fda8f
Bobby Neal <rsneal01@gmail.com>
2020-04-29 16:39:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(deli, name)
     puts "Welcome, #{name}. You are number #{deli.length} in line."
   end
   
-def now_serving()
+def now_serving(deli)
 
 
 # index = deli.index(name)
