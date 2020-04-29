183e5bbccf390e2d95f3dd2a7799935d756d7f9e
Bobby Neal <rsneal01@gmail.com>
2020-04-29 16:41:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,9 @@ def take_a_number(deli, name)
   end
   
 def now_serving(deli)
-  puts "There is nobody waiting to be served!"
+  if deli.length == 0
+    puts "There is nobody waiting to be served!"
+  end
 end
 
 
